b5f4a2e467cab7c2cf061e191eb8db247d34f457
mart31130 <78817145+mart31130@users.noreply.github.com>
2022-12-12 19:36:17 +0100

    JeuNombreADeviner

--- JeuNombreADeviner.h
+++ JeuNombreADeviner.h
@@ -1,19 +1,20 @@
 /*************************************************/
-// Nom du projet: Jeu du nombre ‡ deviner
+// Nom du projet: Jeu du nombre √† deviner
 // Nom du fichier: JeuNombreADeviner.h
 // Version : 1.0
 // Nom du programmeur: MP Pinaud
-// Date de crÈation : 25/11/2022
-// RÙle du fichier: Contient la dÈclaration des fonctions concernant le jeu
+// Date de cr√©ation : 25/11/2022
+// R√¥le du fichier: Contient la d√©claration des fonctions concernant le jeu
 // Nom des composants utilises:
 // Historique du fichier:
 /*************************************************/
-
+// Nom du programmeur: Daignan Martin
+// modification: ajout de la d√©claration de la fonction jouerPartie
 
 // Nom :tirerNombreMystere
-// RÙle : Tire alÈatoirement un nombre ‡ deviner entre nbMin et nbMAx
-// ParamËtres d'entrÈe: nbMin, nbMAx : limites du nombre ‡ deviner
-// Valeur de retour : nombre ‡ deviner
+// R√¥le : Tire al√©atoirement un nombre √† deviner entre nbMin et nbMAx
+// Param√®tres d'entr√©e: nbMin, nbMAx : limites du nombre √† deviner
+// Valeur de retour : nombre √† deviner
 
 int tirerNombreMystere(int nbMin, int nbMax);
 int jouerPartie(int number,int nb_min,  int nb_max , int nb_essai);
@@ -22,12 +23,12 @@ int jouerPartie(int number,int nb_min,  int nb_max , int nb_essai);
 
 
 // Nom :jouerPartie
-// RÙle : Fait jouer une partie ‡ un joueur
-// ParamËtres d'entrÈe: le nombre ‡ deviner,
-//                      les limites min et max du nombre ‡ deviner,
+// R√¥le : Fait jouer une partie √† un joueur
+// Param√®tres d'entr√©e: le nombre √† deviner,
+//                      les limites min et max du nombre √† deviner,
 //                      le nombre maximal d'essais possibles
 // Valeur de retour :   si la partie est perdue, la valeur de retour est -1
-//                      si la partie est gagnÈe, la valeur de retour est le nombre d'essais utilisÈs pour trouver le nombre
+//                      si la partie est gagn√©e, la valeur de retour est le nombre d'essais utilis√©s pour trouver le nombre
 
 
 // Ajouter la DECLARATION de la fonction
